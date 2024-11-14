ad9791c5b7f3d54ac186f1c62fe4a48951e8deb5
Subash Bhattachan <bash_shoe@hotmail.com>
2024-11-13 23:18:11 -0500

    second commit

--- index.js
+++ index.js
@@ -2,13 +2,79 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello Express!");
-});
 
-app.get("/express", (req, res) => {
-    res.send("Creating routes with Express is simple!");
-});
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('form');
+})
+
+app.get('/home', function(req, res) {
+    res.render('home');
+})
+
+
+// app.get('/home', function(req, res) {
+//     res.render('home', {
+//         name: "Programming",
+//         url: "http://wwww.google.com"
+//     });
+// });
+
+// app.get('/home', function(req, res) {
+//     res.render('home', {
+//         title:"Home Page",
+//         users:{name: "Subash", age: "25"}
+//     });
+// });
+
+
+
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Express!");
+// });
+
+// app.get("/express", (req, res) => {
+//     res.send("Creating routes with Express is simple!");
+// });
+
+// app.get("/user", (req, res) => {
+//     res.send("Received a GET request for user!")
+// })
+
+// app.post("/user", (req, res) => {
+//     res.send("Received a POST request for user!")
+// })
+
+
+// app.get("/", (req, res) => {
+//     res.send("Try navigating to /user.");
+// });
+
+// app.get("/user", (req, res) => {
+//     res.send(`Received a GET request for user! Try navigating to /user/somevalue/profile/somevalue.`);
+// });
+
+// app.get("/user/:userID", (req, res) => {
+//     res.send(`Navigated to the user page for: ${req.params.userID}.`);
+// });
+
+// app.get("/user/:userID/profile", (req, res) => {
+//     res.send(`Navigated to the user profile page for: ${req.params.userID}.`);
+// });
+
+// app.get("/user/:userID/profile/:data", (req, res) => {
+//     res.send(`Navigated to the user profile page for: ${req.params.userID}, with the data: ${req.params.data}.`);
+// });
+
+
 
 
 app.listen(port, () => {
